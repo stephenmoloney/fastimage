689063222a50186d9abbc9e4ca09243c27c36ca5
Boris Mikhaylov <kaguxmail@gmail.com>
2016-08-13 01:22:02 +0200

    simplified redirects handling

--- lib/fastimage.ex
+++ lib/fastimage.ex
@@ -72,14 +72,7 @@ defmodule Fastimage do
 
   defp recv(url, :url, num_chunks) do
     {:ok, stream_ref} = :hackney.get(url, [], <<>>, [{:async, :once}, {:follow_redirect, true}])
-    response = stream_chunks(stream_ref, num_chunks, {0, <<>>}) # returns {:ok, data, ref}
-    case response do
-      {:redirect, to_url} ->
-        close_stream(stream_ref)
-        recv(to_url, :url, num_chunks)
-      _ ->
-        response
-    end
+    stream_chunks(stream_ref, num_chunks, {0, <<>>}) # returns {:ok, data, ref}
   end
 
 
@@ -112,7 +105,7 @@ defmodule Fastimage do
           {:hackney_response, stream_ref, {:headers, _headers}} ->
             stream_chunks(stream_ref, num_chunks_to_fetch, {acc_num_chunks, acc_data})
           {:hackney_response, _stream_ref, {:redirect, to_url, _headers}} ->
-            {:redirect, to_url}
+            recv(to_url, :url, num_chunks_to_fetch)
           {:hackney_response, stream_ref, :done} ->
             {:ok, acc_data, stream_ref}
           {:hackney_response, stream_ref, data} ->
